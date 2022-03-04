ab7538f521c654228a387e48c37781c795320748
fdonkar <fdonkar@gmail.com>
2022-03-04 12:32:05 +0300

    add scenario to DeleteCar.feature

--- src/test/java/com/translantik/step_definitions/DeleteCarStepDefinitions.java
+++ src/test/java/com/translantik/step_definitions/DeleteCarStepDefinitions.java
@@ -15,9 +15,9 @@ public class DeleteCarStepDefinitions {
 
     VehiclesPage vehiclesPage = new VehiclesPage();
 
-    @When("the user hovers over the three dots")
-    public void theUserHoversOverTheThreeDots() {
-        vehiclesPage.goThreeDot("Driver","Julius Agahowa");
+    @When("the user hovers over the three dots of row whose {string} equals {string}")
+    public void theUserHoversOverTheThreeDotsOfRowWhoseEquals(String columnName, String value) {
+        vehiclesPage.goThreeDot(columnName,value);
     }
 
     @Then("the user should be able to see the delete button")
@@ -34,5 +34,18 @@ public class DeleteCarStepDefinitions {
     @Then("The Delete Confirmation pop up should be displayed")
     public void theDeleteConfirmationPopUpShouldBeDisplayed() {
         Assert.assertTrue("Delete Confirmation pop up is NOT displayed ",vehiclesPage.deleteConfirmation.isDisplayed());
+    }
+
+    @And("the user clicks on the -Yes,Delete- confirmation button")
+    public void theUserClicksOnTheYesDeleteConfirmationButton() {
+        BrowserUtils.waitForVisibility(vehiclesPage.deleteConfirmation,5);
+        vehiclesPage.yesDeleteButton.click();
+    }
+
+    @Then("the user should delete the car and {string} message should be displayed")
+    public void theUserShouldDeleteTheCarAndMessageShouldBeDisplayed(String expectedMessage) {
+        vehiclesPage.waitUntilLoaderScreenDisappear();
+        String actualMessage = vehiclesPage.itemDeletedMessage.getText();
+        Assert.assertEquals("Message is NOT as expected",expectedMessage,actualMessage);
     }
 }
--- src/test/resources/features/DeleteCar.feature
+++ src/test/resources/features/DeleteCar.feature
@@ -2,12 +2,20 @@ Feature: Delete a car
 
 
   Scenario: The user should be able to see the delete button by hovering over the three dots at the end of each row
-    When the user hovers over the three dots
+    When the user hovers over the three dots of row whose "ChassisNumber" equals "117,312"
     Then the user should be able to see the delete button
-  @wip
+
   Scenario: "Delete Confirmation" pop up should be displayed when the user clicks on the delete button
-    When the user hovers over the three dots
+    When the user hovers over the three dots of row whose "ChassisNumber" equals "117,312"
     And the user clicks on the delete button
     Then The Delete Confirmation pop up should be displayed
+
+  @wip
+  Scenario: The user should be able to delete any car by clicking on the delete button at the end of each row
+    When the user hovers over the three dots of row whose "ChassisNumber" equals "117,312"
+    And the user clicks on the delete button
+    And the user clicks on the -Yes,Delete- confirmation button
+    Then the user should delete the car and "Item deleted" message should be displayed
+
 
 
