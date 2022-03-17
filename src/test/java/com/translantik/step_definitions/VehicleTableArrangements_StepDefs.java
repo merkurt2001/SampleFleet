8163828ba7857316e5b8720815a525b7f99f40d4
RamazanArs <ramazan.arslan11@gmail.com>
2022-03-17 18:20:32 +0300

    US004 is completed

--- src/test/java/com/translantik/step_definitions/VehicleTableArrangements_StepDefs.java
+++ src/test/java/com/translantik/step_definitions/VehicleTableArrangements_StepDefs.java
@@ -3,10 +3,12 @@ package com.translantik.step_definitions;
 import com.translantik.pages.VehiclesPage;
 import com.translantik.utilities.BrowserUtils;
 import com.translantik.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -16,45 +18,89 @@ import java.util.List;
 
 public class VehicleTableArrangements_StepDefs {
 
-    VehiclesPage vehiclesPage=new VehiclesPage();
-
-
-    @Then("options should be same with below")
-    public void options_should_be_same_with_below (List < String > expectedOptions) {
-        BrowserUtils.waitFor(3);
-        List<String> actualList = new ArrayList<>();
-
-        List<WebElement> elements = Driver.get().findElements(By.xpath("//a[@class='dropdown-item']"));
-
-        BrowserUtils.waitFor(2);
-
-        for (WebElement element : elements) {
-            String innerText = element.getAttribute("innerText");
-            String lastText=innerText.trim();
-            actualList.add(lastText);
-            BrowserUtils.waitFor(1);
-        }
-
-
-        System.out.println("expectedList = " + expectedOptions);
-        System.out.println("actualList = " + actualList);
-        Assert.assertEquals(expectedOptions,actualList);
-
-
-
-    }
-
+    VehiclesPage vehiclesPage = new VehiclesPage();
 
 
     @Then("the View per page should be {string} default")
-    public void theViewPerPageShouldBeDefault( String expected) {
+    public void theViewPerPageShouldBeDefault(String expected) {
         BrowserUtils.waitFor(5);
 
-        String actual = Driver.get().findElement(By.xpath("//button[@class='btn dropdown-toggle ']")).getText();
+//        String actual = Driver.get().findElement(By.xpath("//button[@class='btn dropdown-toggle ']")).getText();
 
+        String actual = vehiclesPage.viewPerPageButton.getText();
         System.out.println("actual = " + actual);
         System.out.println("expected = " + expected);
 
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Then("options should be same with below")
+    public void options_should_be_same_with_below(List<String> expectedOptions) {
+
+        BrowserUtils.waitFor(1);
+
+        List<String> actualList = BrowserUtils.getElementsText(vehiclesPage.viewfilter);
+        Assert.assertEquals(expectedOptions, actualList);
+
+        System.out.println("expectedList = " + expectedOptions);
+        System.out.println("actualList = " + actualList);
+        Assert.assertEquals(expectedOptions, actualList);
+    }
+
+    @When("the user click on View per page button")
+    public void theUserClickOnButton() {
+        vehiclesPage.viewPerPageButton.click();
+    }
+
+    @When("the user clicks on {int} to arrange the row numbers of the results")
+    public void the_user_clicks_on_to_arrange_the_row_numbers_of_the_results(Integer int1) {
+        WebElement viewPerPageOptions = Driver.get().findElement(By.xpath("//a[normalize-space()='" + int1 + "']"));
+        viewPerPageOptions.click();
+    }
+
+    @Then("the rows should be equal or less than the {int}")
+    public void theRowsShouldBeEqualOrLessThanTheValue(int expected) {
+        BrowserUtils.waitFor(3);
+        System.out.println("actual=" + getNumberOfRows(expected));
+        System.out.println("expected=" + expected);
+
+        Assert.assertEquals(expected, getNumberOfRows(expected));
+    }
+
+    public int getNumberOfRows(int a) {
+        List<String> allRowsWithoutHeader = BrowserUtils.getElementsText(vehiclesPage.vehicleTableRows);
+        return allRowsWithoutHeader.size();
+    }
+
+
+    @When("the user click the Model Year button")
+    public void the_user_click_the_Model_Year_button() {
+        BrowserUtils.waitFor(3);
+        JavascriptExecutor executor = (JavascriptExecutor) Driver.get();
+        executor.executeScript("arguments[0].click();", vehiclesPage.modelYearLocator);
+
+
+    }
+
+    @Then("the user can sort a column in ascending or descending order")
+    public void the_user_can_sort_a_column_in_ascending_or_descending_order() {
+
+    }
+
+    @Then("the user click the reset button")
+    public void the_user_click_the_reset_button() {
+        BrowserUtils.waitFor(3);
+        vehiclesPage.resetButton_Locator.click();
+        BrowserUtils.waitFor(3);
+    }
+
+    @Then("the user can remove all sortings and filterings on the page")
+    public void the_user_can_remove_all_sortings_and_filterings_on_the_page() {
+        for (WebElement a : vehiclesPage.spanCaret_Locator) {
+            Assert.assertFalse(a.isDisplayed());
+        }
     }
 }
+
+
+
--- src/test/resources/features/VehicleTableArrangements.feature
+++ src/test/resources/features/VehicleTableArrangements.feature
@@ -4,17 +4,41 @@
 #    Background:
 #      Given the user is on the vehicles page
 #
-      @US004_AC1 @RA
+      @TRANS-1280 @US004_AC1 @RA
     Scenario: The value of 'View Per Page' should be '25' by default
 #      When the store manager clicks the "View per page" button
       Then the View per page should be '25' default
 
-    @US004_AC2 @RA
+    @TRANS-1281 @US004_AC2 @RA
       Scenario: 'View Per Page' includes the values shown as below
-#     When the user click on "View per page" button
+     When the user click on View per page button
       Then options should be same with below
         |10 |
         |25 |
         |50 |
         |100|
+
+    @TRANS-1282 @US004_AC3 @RA
+      Scenario Outline:'View Per Page' button under the Fleet-Vehicles module displayed truely
+        When the user click on View per page button
+        And the user clicks on <value> to arrange the row numbers of the results
+        Then the rows should be equal or less than the <value>
+        Examples:
+        |value|
+        |10   |
+        |25   |
+        |50   |
+
+    @TRANS-1283 @US004_AC4 @RA
+        Scenario: User can sort a column in ascending or descending order by clicking the column name
+          When the user click the Model Year button
+          Then the user can sort a column in ascending or descending order
+
+    @TRANS-1284 @US004_AC5 @RA
+    Scenario:   User can remove all sortings and filterings on the page by using the reset button
+      When the user click the Model Year button
+      Then the user click the reset button
+      Then the user can remove all sortings and filterings on the page
+
+
 
